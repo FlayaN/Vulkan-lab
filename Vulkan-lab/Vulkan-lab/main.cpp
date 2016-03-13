02d3cda530c6648ace97e0c3eec826ef904bcda0
FlayaN <hannes.feldt@gmail.com>
2016-03-13 20:15:59 +0100

    Enumerate devices

--- Vulkan-lab/Vulkan-lab/main.cpp
+++ Vulkan-lab/Vulkan-lab/main.cpp
@@ -3,6 +3,54 @@
 
 int main()
 {
+	vk::ApplicationInfo appInfo;
+	appInfo.pApplicationName("VoxelWorldTestRenderApp");
+	appInfo.pEngineName("VoxelWorld");
+
+	appInfo.apiVersion(VK_API_VERSION);
+
+	vk::InstanceCreateInfo createInfo;
+	createInfo.pApplicationInfo(&appInfo);
+	//vk::AllocationCallbacks allocationCallbacks;
+	vk::Instance instance;
+	vk::Result result = vk::createInstance(&createInfo, nullptr, &instance);
+
+
+	std::vector<vk::PhysicalDevice> devices;
+	devices = instance.enumeratePhysicalDevices();
+
+	////std::string graphicsCardName = 
+	//auto ddsa = devices[0].getProperties();
+
+	//auto tmp = devices[0].getMemoryProperties();
+
+	vk::Device device;
+	vk::DeviceCreateInfo deviceCreateInfo;
+	//deviceCreateInfo.queueCreateInfoCount(1);
+
+	vk::DeviceQueueCreateInfo deviceQueueCreateInfo;
+	//deviceQueueCreateInfo.
+
+	deviceCreateInfo.pQueueCreateInfos(&deviceQueueCreateInfo);
+
+	auto physicalDevice = devices[0];
+
+	result = physicalDevice.createDevice(&deviceCreateInfo, nullptr, &device);
+
+	std::vector<vk::QueueFamilyProperties> queueProps;
+	//queueProps.resize(queueCount);
+
+	queueProps = physicalDevice.getQueueFamilyProperties();
+
+	////vkGetPhysicalDeviceQueueFamilyProperties(physicalDevice, &queueCount, queueProps.data());
+
+	for(auto& queueProp : queueProps)
+	{
+		if (queueProp.queueFlags() & vk::QueueFlagBits::eGraphics)
+			break;
+	}
+
+
 
 	return 1;
 }
